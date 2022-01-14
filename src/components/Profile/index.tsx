3f89c9ba99a17a1579d0e9b10c59405edfde381c
Adson Novaes <adsonnovaes7@gmail.com>
2022-01-14 15:59:41 -0300

    Displaying the logged in user data

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../hooks/auth";
 
 import { Avatar } from "../Avatar";
 
 import { styles } from './styles';
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
 
-      <Avatar urlImage="https://github.com/adsonnovaes.png"/>
+      <Avatar urlImage={user.avatar}/>
 
       <View>
 
@@ -19,7 +22,7 @@ export function Profile() {
           </Text>
 
           <Text style={styles.username}>
-            Adson
+            { user.firstName }
           </Text>
         </View>
 
