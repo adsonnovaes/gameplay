d6503b6801dd08260099421241f20596bc2ced03
Adson Novaes <adsonnovaes7@gmail.com>
2022-01-14 15:48:11 -0300

    Public and private routes

--- src/Routes/auth.routes.tsx
+++ src/Routes/auth.routes.tsx
@@ -4,7 +4,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { theme } from '../global/styles/theme';
 
 import { Home } from '../screens/Home';
-import { SignIn } from '../screens/Signin';
 import { AppointmentDetails } from '../screens/AppointmentDetails';
 import { AppointmentCreate } from '../screens/AppointmentCreate';
 
@@ -20,10 +19,6 @@ export function AuthRoutes() {
         }
       }}
     >
-      <Screen 
-        name="SignIn"
-        component={SignIn}
-      />
       <Screen
         name="Home"
         component={Home}
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AuthRoutes } from './auth.routes';
+import { useAuth } from "../hooks/auth";
+import { SignIn } from "../screens/Signin";
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
+      {user.id ? <AuthRoutes /> : <SignIn/>}
     </NavigationContainer>
   );
 }
